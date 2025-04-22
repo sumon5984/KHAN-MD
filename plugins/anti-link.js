2525819da553ffefb0b18c7adbad5b4c0b58d97c
KHAN MD <157266962+XdTechPro@users.noreply.github.com>
2025-04-22 11:11:19 +0500

    Update anti-link.js

--- plugins/anti-link.js
+++ plugins/anti-link.js
@@ -66,7 +66,7 @@ cmd({
       if (warningCount < 4) {
         // Send warning message
         await conn.sendMessage(from, {
-          text: `‎*⚠️ LINKS ARE NOT ALLOWED ⚠️*\n` +
+          text: `‎*⚠️LINKS ARE NOT ALLOWED⚠️*\n` +
                 `*╭────⬡ WARNING ⬡────*\n` +
                 `*├▢ USER :* @${sender.split('@')[0]}!\n` +
                 `*├▢ COUNT : ${warningCount}*\n` +
