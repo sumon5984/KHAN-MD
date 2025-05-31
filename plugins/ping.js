7532141edccd44545a362d9119f4cc9fce453079
sumon5984 <sbot7603@gmail.com>
2025-05-31 20:58:35 +0530

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -41,8 +41,8 @@ async (conn, mek, m, { from, quoted, sender, reply }) => {
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363354023106228@newsletter',
-                    newsletterName: "JawadTechX",
+                    newsletterJid: '12036335r',
+                    newsletterName: "J",
                     serverMessageId: 143
                 }
             }
